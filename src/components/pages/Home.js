6b234e117c62a8022ceab21807d21636b4975626
ornel77 <t.ornel@yahoo.fr>
2020-01-13 11:49:32 +0100

    component Home and NotFund added, ready to DEPLOY

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Search from '../users/Search'
+import Users from '../users/Users'
+
+const Home = props => (
+    <Fragment>
+        <Search/>
+        <Users/>
+    </Fragment>
+)
+
+
+export default Home
